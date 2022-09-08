39454a646c0a9cb1bababdcfb1af26e1bf169d9d
vijay.krishna <vijay.krishna@quantilope.com>
2022-09-08 13:49:50 +0200

    feat(Survey2.0): deploy static site to netlify

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,8 @@ import react from '@astrojs/react';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next(), react(), tailwind()],
-  output: "server",
-  adapter: netlify()
 });
